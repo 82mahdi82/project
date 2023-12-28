085467bd92a1f32ff7a9f456fce4f89740364db6
82mahdi82 <141176041+82mahdi82@users.noreply.github.com>
2023-12-29 00:21:41 +0330

    s15

--- main.py
+++ main.py
@@ -124,21 +124,21 @@ def show_cart(cid):
     # markup2.add(InlineKeyboardButton(
     #             "خرید و ثبت نهایی", callback_data="shop_cart"))
     list_shoppingcart=database.use_shopping_cart_table_where(f"cid={cid}")
+    if len(list_shoppingcart)==0:
+        markup = ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(KeyboardButton("منوی اصلی "))
+        return bot.edit_message_text("سبد خرید شما خالی است", cid,sssss,reply_markup=markup)
     price_total=0
     for b in list_shoppingcart:
         list_price=database.use_product_table_where(f"product_id={b['product_id']}")
         for i in list_price:
             price_total+=i["price"]*b["qty"]
-    if price_total==0:
-        markup = ReplyKeyboardMarkup(resize_keyboard=True)
-        markup.add(KeyboardButton("منوی اصلی "))
-        return bot.edit_message_text("سبد خرید شما خالی است", cid,reply_markup=markup)
-    else:
-        markup2 = InlineKeyboardMarkup()
-        markup2.add(InlineKeyboardButton(
-                    "خرید و ثبت نهایی", callback_data="shop_cart"))
-        print(price_total)
-        return bot.edit_message_text(pricr_cart(price_total), cid, sssss, reply_markup=markup2)
+    
+    markup2 = InlineKeyboardMarkup()
+    markup2.add(InlineKeyboardButton(
+                "خرید و ثبت نهایی", callback_data="shop_cart"))
+    print(price_total)
+    return bot.edit_message_text(pricr_cart(price_total), cid, sssss, reply_markup=markup2)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith("cart"))
