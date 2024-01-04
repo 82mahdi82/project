f1ba8121ef89e9ce2b1d4e6ba7bb7bc78b37b808
82mahdi82 <141176041+82mahdi82@users.noreply.github.com>
2024-01-04 22:49:57 +0330

    akhari

--- main.py
+++ main.py
@@ -1,13 +1,134 @@
+import spotipy
 from pytube import YouTube
+from spotipy.oauth2 import SpotifyClientCredentials
+import requests
+from youtubesearchpython import VideosSearch
+from pprint import pprint
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 
-# لینک یوتیوب را وارد کنید
-youtube_link = "https://youtu.be/-dthTiyaPRw?si=J7pdnzEkY6gEqUKr"
 
-# دانلود صدا با سرعت بالا
-yt = YouTube(youtube_link)
-audio_stream = yt.streams.filter(only_audio=True, file_extension='mp4').first()
-audio_stream.download(output_path='.', filename='audio')
+TOKEN = '6317356905:AAGQ2p8Lo0Kc4mkChTmE7ZbI2p1bzw9cIO8'
+# تنظیمات API Spotify
+client_id = 'c4821cf4363c4c438110e04b0d50f59b'
+client_secret = 'd8eac638fd604aa8849f6f929ef8b538'
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+bot = telebot.TeleBot(TOKEN)
 
-# تغییر پسوند فایل به mp3
-import os
-os.rename("audio.mp4", "audio.mp3")
+
+userStep = {}
+
+def get_user_step(uid):
+    if uid in userStep:
+        return userStep[uid]
+    else:
+        userStep[uid] = 0
+        print("New user detected, who hasn't used \"/start\" yet")
+        return 0
+    
+def download_artist_tracks(artist_name,cid):
+    # جستجوی هنرمند بر اساس نام
+    results = sp.search(q=artist_name, type='artist')
+
+    if results['artists']['items']:
+        artist_id = results['artists']['items'][0]['id']
+
+        # دریافت تمام آهنگ‌های هنرمند
+        tracks = sp.artist_top_tracks(artist_id)
+
+        # دانلود تمام آهنگ‌ها
+        for track in tracks['tracks']:
+            track_name = track['name']
+            print(track_name)
+            get_youtube_track_url(track_name,cid)
+            break
+            # track_id = track['id']
+            # download_track(track_id, f"{artist_name} - {track_name}")
+    else:
+        print(f"هیچ هنرمندی با نام {artist_name} یافت نشد.")
+
+def get_youtube_track_url(song_name,cid):
+    try:
+        # جستجو در YouTube
+        videos_search = VideosSearch(song_name, limit = 1)
+        results = videos_search.result()
+
+        # چک کردن آیا نتیجه وجود دارد یا خیر
+        if results['result']:
+            # گرفتن لینک اولین ویدئو
+            video_url = results['result'][0]['link']
+            video_do(video_url,cid)
+        else:
+            return None
+    except Exception as e:
+        print(f"خطا در دریافت لینک از YouTube: {e}")
+
+
+def video_do(url,cid):
+    yt = YouTube(url)
+    try:
+        # print("P")
+        video = yt.streaming_data
+        # print(video["formats"])
+        # print(video["formats"][0]["url"])
+        # print("_"*20)
+        voice= yt.streaming_data
+        pprint(voice["adaptiveFormats"])
+        # for i in voice["adaptiveFormats"]:
+        #     print(i["mimeType"])
+        #     if i["mimeType"].startswith('audio/mp4; codecs="mp4a.40.5"'):
+        #         print(i["url"])
+        #         download_video(i["url"],"alliiik.mp3")
+        #     elif i["mimeType"].startswith('audio/webm; codecs="opus"'):
+        #         print(i["url"])
+        #         download_video(i["url"],"alliiik2.mp3")
+        #         break
+
+        # print(voice["adaptiveFormats"][12]["url"])
+        download_video(voice["adaptiveFormats"][-1]["url"],cid,"hoooooooooooy.mp3")
+
+    except:
+        print("noo")
+
+
+
+def download_video(url,cid, output_path='videooooooo.mp4'):
+    try:
+        # درخواست GET برای دریافت داده‌های ویدئو
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        bot.send_message(cid,"در حال ارسال آهنگ")
+        bot.send_audio(cid,response.content)
+        # باز کردن یک فایل برای ذخیره ویدئو
+        with open(output_path, 'wb') as video_file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    video_file.write(chunk)
+
+        print(f"ویدئو با موفقیت در {output_path} ذخیره شد.")
+
+    except requests.exceptions.HTTPError as errh:
+        print ("خطا HTTP:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print ("خطا اتصال:", errc)
+    except requests.exceptions.Timeout as errt:
+        print ("خطا Timeout:", errt)
+    except requests.exceptions.RequestException as err:
+        print ("خطای نامشخص:", err)
+
+
+
+@bot.message_handler(commands=['start'])
+def command_start(m):
+    cid = m.chat.id
+    bot.send_message(cid,"لطفا اسم آهنگ مورد نظر خود را ارسال کنید")
+    userStep[cid]=1
+
+@bot.message_handler(func=lambda m: get_user_step(m.chat.id)==1)
+def send_music(m):
+    cid=m.chat.id
+    download_artist_tracks(m.text,cid)
+
+bot.infinity_polling()
+# تست کد با نام هنرمند مورد نظر
+# download_artist_tracks('greedy')
